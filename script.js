93a644bf0462831e42906571f76ab50a840bf840
Chris Religa <chris.religa@gmail.com>
2025-08-29 17:32:43 -0700

    Add icon functionality and update task counters
    - Implement click handlers for complete, trash, and flag icons.
    - Pending and completed task numbers now update correctly.

--- script.js
+++ script.js
@@ -4,17 +4,26 @@
 const addTaskButton = document.getElementById('add-task-button');
 const taskList = document.getElementById('task-list');
 const taskInput = document.getElementById('taskInput');
+const pendingTask = document.getElementById('pendingTasks');
+const completedTask = document.getElementById('completedTasks');
 
+completedTask.textContent = 0;
+pendingTask.textContent = 0;
+//
+//
+// Add A New Task-Click
 addTaskButton.addEventListener('click', function () {
   const text = taskInput.value;
 
   const newDiv = document.createElement('div');
+  newDiv.classList.add('newDiv');
   taskList.appendChild(newDiv);
 
   const newItem = document.createElement('li');
   newItem.textContent = text;
 
   const iconDiv = document.createElement('div');
+  iconDiv.classList.add('iconDiv');
   const trashCan = document.createElement('i');
   trashCan.classList.add('fa-solid', 'fa-trash');
   const checkMark = document.createElement('i');
@@ -29,20 +38,26 @@ addTaskButton.addEventListener('click', function () {
   newDiv.appendChild(newItem);
   newDiv.appendChild(iconDiv);
 
+  pendingTask.textContent++;
+
   taskInput.value = '';
 });
-
+//
+//
+// Add A New Task-Enter Key
 taskInput.addEventListener('keydown', function (event) {
   if (event.key === 'Enter') {
     const text = taskInput.value;
 
     const newDiv = document.createElement('div');
+    newDiv.classList.add('newDiv');
     taskList.appendChild(newDiv);
 
     const newItem = document.createElement('li');
     newItem.textContent = text;
 
     const iconDiv = document.createElement('div');
+    iconDiv.classList.add('iconDiv');
     const trashCan = document.createElement('i');
     trashCan.classList.add('fa-solid', 'fa-trash');
     const checkMark = document.createElement('i');
@@ -57,6 +72,27 @@ taskInput.addEventListener('keydown', function (event) {
     newDiv.appendChild(newItem);
     newDiv.appendChild(iconDiv);
 
+    pendingTask.textContent++;
+
     taskInput.value = '';
+  }
+});
+//
+//
+// Icons (Complete, Delete, Flag)
+taskList.addEventListener('click', (e) => {
+  let task = e.target.closest('.newDiv');
+  let icons = e.target.closest('.iconDiv');
+
+  if (e.target.classList.contains('fa-square-check')) {
+    completedTask.textContent++;
+    pendingTask.textContent--;
+    task.remove();
+  } else if (e.target.classList.contains('fa-trash')) {
+    pendingTask.textContent--;
+    task.remove();
+  } else if (e.target.classList.contains('fa-flag')) {
+    task.style.backgroundColor = 'rgba(246, 255, 0, 0.76)';
+    icons.style.backgroundColor = 'rgba(246, 255, 0, 0)';
   }
 });
