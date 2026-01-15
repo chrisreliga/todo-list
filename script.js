4d85f03e497f68789c99933dcb52104119d0a927
Chris Religa <chris.religa@gmail.com>
2026-01-14 22:19:02 -0800

    WIP: added task list & Add Task+ button functionality to dynamically create more tasks when enter key is pressed or user clicks out of the text input

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
 'use strict';
 
 const addTaskBtn = document.querySelector('.add-task-btn');
-const newTaskInput = document.querySelector('.new-task-input');
 const textInput = document.getElementById('task-input');
+const taskInputContainer = document.querySelector('.task-input-container');
+
+let tasksArray = [];
+
+function renderTasks() {
+  const taskList = document.querySelector('.task-list');
+  taskList.innerHTML = '';
+
+  tasksArray.forEach((task) => {
+    const li = document.createElement('li');
+    li.className = 'task-list-item';
+    li.textContent = task;
+    taskList.appendChild(li);
+  });
+}
+
+function addTask() {
+  const newTask = textInput.value.trim();
+  if (!newTask) return;
+
+  tasksArray.unshift(newTask);
+  renderTasks();
+
+  textInput.value = '';
+  taskInputContainer.classList.remove('show');
+}
 
 addTaskBtn.addEventListener('click', () => {
-  newTaskInput.classList.add('show');
-
+  taskInputContainer.classList.add('show');
   textInput.focus();
 });
+
+textInput.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') addTask();
+});
+
+textInput.addEventListener('blur', addTask());
