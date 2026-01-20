1846015328a6d585eb15de9e4b23754ff860da73
Chris Religa <chris.religa@gmail.com>
2026-01-19 21:08:36 -0800

    fix: the checkbox now removes a task when it's correlating checkbox is clicked

--- script.js
+++ script.js
@@ -4,15 +4,30 @@ const addTaskBtn = document.querySelector('.add-task-btn');
 const textInput = document.getElementById('task-input');
 const inputRow = document.querySelector('.task-input-container');
 
+let tasksArray = [];
+let taskIdCounter = 0;
+
+const taskList = document.querySelector('.task-list');
+//
+// Checking off & removing a task by clicking checkbox
+taskList.addEventListener('click', (e) => {
+  if (e.target.classList.contains('checkbox')) {
+    const btn = e.target.closest('.completed-btn');
+    if (!btn) return;
+    const id = Number(btn.dataset.id);
+
+    tasksArray = tasksArray.filter((task) => task.id !== id);
+    renderTasks();
+  }
+});
+//
+//
 window.addEventListener('DOMContentLoaded', () => {
   textInput.focus();
 });
-
-let tasksArray = [];
 //
 //
-//
-//
+// Render Tasks Function
 function renderTasks() {
   const taskList = document.querySelector('.task-list');
 
@@ -22,12 +37,14 @@ function renderTasks() {
   taskList.appendChild(inputRow);
   //
   //
+  // Creating the task list item elements
   tasksArray.forEach((taskObj) => {
     const li = document.createElement('li');
     li.className = 'task-list-item';
 
     const completedBtn = document.createElement('button');
     completedBtn.classList.add('completed-btn');
+    completedBtn.dataset.id = taskObj.id;
 
     const icon = document.createElement('i');
     icon.className = 'fa-regular fa-circle checkbox';
@@ -43,24 +60,20 @@ function renderTasks() {
 }
 //
 //
-//
-//
+// Add Task Function
 function addTask() {
   const newTask = textInput.value.trim();
   if (!newTask) return;
 
-  tasksArray.unshift({ text: newTask, completed: false });
+  tasksArray.unshift({ id: taskIdCounter++, text: newTask, completed: false });
   renderTasks();
 
   textInput.value = '';
   inputRow.classList.remove('show');
-
-  console.log(tasksArray[1]);
 }
 //
 //
-//
-//
+// Event Handlers
 addTaskBtn.addEventListener('click', () => {
   inputRow.classList.add('show');
   textInput.focus();
